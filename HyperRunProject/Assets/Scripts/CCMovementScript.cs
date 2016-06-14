cdd95717a78d82552d17be25b7a1baac812c1ab8
ckinne1771 <ckinney1771@gmail.com>
2016-06-14 18:57:09 +0100

    basic Jumping added
    TODO: Fix jumping and gravity, disable movement in air

--- HyperRunProject/Assets/Scripts/CCMovementScript.cs
+++ HyperRunProject/Assets/Scripts/CCMovementScript.cs
@@ -5,15 +5,15 @@ using System.Collections;
 public class CCMovementScript : MonoBehaviour
 {
 
-   
+
     public Transform CameraTransform;
     // public float speed = 2.0f;
     public float topSpeed = 10f;
     public float speed = 1.0f;
-    public float jumpSpeed = 1.0f;
-    public float gravity = 20.0f;
-    float drag = 50.0f;
-    float diagonalDrag = 75.0f;
+    public float jumpSpeed = 20.0f;
+    private float gravity = 120.0f;
+    float drag = 100.0f;
+    float diagonalDrag = 125.0f;
 
     private bool isMoving = false;
     private Vector3 moveDirection = Vector3.zero;
@@ -21,73 +21,76 @@ public class CCMovementScript : MonoBehaviour
     private Rigidbody myrigidbody;
     private CharacterController controller;
 
+    public static bool isGrounded = false;
     // Update is called once per frame
     void Start()
     {
         myrigidbody = this.GetComponent<Rigidbody>();
         myrigidbody.freezeRotation = true;
         controller = this.GetComponent<CharacterController>();
-   
+
     }
-  
-    bool IsGrounded()
-    {
-        if(Physics.Raycast(transform.position, -transform.up, 2f))
-        {
-            Debug.Log("hitting");
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
+
 
     void Update()
     {
-        float horizontalInput = Input.GetAxisRaw("Horizontal")*  speed;
-        float verticalInput = Input.GetAxisRaw("Vertical")* speed;
-
+        float horizontalInput = 0;
+        float verticalInput = 0;
         
-        Vector3 targetDirection = (horizontalInput * Camera.main.transform.right) + (verticalInput * Camera.main.transform.forward);
-        moveDirection = Vector3.RotateTowards(moveDirection, targetDirection, 200 * Mathf.Deg2Rad * Time.deltaTime, 1000);
+            horizontalInput = Input.GetAxisRaw("Horizontal") * speed*Time.deltaTime;
+            verticalInput = Input.GetAxisRaw("Vertical") * speed*Time.deltaTime;
 
-        if (horizontalInput > 0 && verticalInput > 0)
-        {
-            velocity = Vector3.MoveTowards(velocity, Vector3.zero, diagonalDrag * Time.deltaTime);
-        }
-        else if (horizontalInput > 0 && verticalInput < 0)
-        {
-            velocity = Vector3.MoveTowards(velocity, Vector3.zero, diagonalDrag * Time.deltaTime);
-        }
-        else if (horizontalInput < 0 && verticalInput > 0)
-        {
-            velocity = Vector3.MoveTowards(velocity, Vector3.zero, diagonalDrag * Time.deltaTime);
-        }
-       else  if (horizontalInput < 0 && verticalInput < 0)
-        {
-            velocity = Vector3.MoveTowards(velocity, Vector3.zero, diagonalDrag * Time.deltaTime);
-        }
-        else
-        {
-            velocity = Vector3.MoveTowards(velocity, Vector3.zero, drag * Time.deltaTime);
-        }
-        velocity += moveDirection;
-        if(velocity.magnitude > topSpeed)
-        {
-            velocity = velocity.normalized * topSpeed;
-        }
-      
-        controller.Move((velocity + new Vector3 (0.0f, -9.8f, 0.0f))* Time.deltaTime);
-        Debug.Log(velocity);
+            Vector3 targetDirection = (horizontalInput * Camera.main.transform.right) + (verticalInput * Camera.main.transform.forward);
+            moveDirection = Vector3.RotateTowards(moveDirection, targetDirection, 200 * Mathf.Deg2Rad * Time.deltaTime, 1000);
+
+            if (horizontalInput > 0 && verticalInput > 0)
+            {
+                velocity = Vector3.MoveTowards(velocity, Vector3.zero, diagonalDrag * Time.deltaTime);
+            }
+            else if (horizontalInput > 0 && verticalInput < 0)
+            {
+                velocity = Vector3.MoveTowards(velocity, Vector3.zero, diagonalDrag * Time.deltaTime);
+            }
+            else if (horizontalInput < 0 && verticalInput > 0)
+            {
+                velocity = Vector3.MoveTowards(velocity, Vector3.zero, diagonalDrag * Time.deltaTime);
+            }
+            else if (horizontalInput < 0 && verticalInput < 0)
+            {
+                velocity = Vector3.MoveTowards(velocity, Vector3.zero, diagonalDrag * Time.deltaTime);
+            }
+            else
+            {
+                velocity = Vector3.MoveTowards(velocity, Vector3.zero, drag * Time.deltaTime);
+            }
+            velocity += moveDirection;
+            if (velocity.magnitude > topSpeed)
+            {
+                velocity = velocity.normalized * topSpeed;
+            }
+
+            if (Input.GetButton("Jump"))
+            {
+            if (isGrounded == true)
+            {
+                Debug.Log("jumping");
+                velocity.y = jumpSpeed;
+            }
+            }
+
+            velocity.y -= gravity *Time.deltaTime;
+            controller.Move(velocity * Time.deltaTime);
 
 
-        if (targetDirection != Vector3.zero)
-        {
-            transform.rotation = Quaternion.LookRotation(moveDirection);
-        }
 
+            if (targetDirection != Vector3.zero)
+            {
+                transform.rotation = Quaternion.LookRotation(moveDirection);
+            }
+
+
+        }
     }
-}
+
 
 
---
+++ HyperRunProject/Assets/Scripts/CollisionScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionScript : MonoBehaviour {
+
+	void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag =="Floor")
+        {
+            CCMovementScript.isGrounded = true;
+        }
+        if(other.gameObject.tag == "RunnableWall")
+        {
+            CCMovementScript.isGrounded = true;
+        }
+    }
+ 
+}
