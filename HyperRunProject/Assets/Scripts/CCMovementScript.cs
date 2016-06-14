b9ac2ffd812fa5d8e3e4033d4d7fd8539718fcd8
ckinne1771 <ckinney1771@gmail.com>
2016-06-14 18:02:34 +0100

    First github Upload
    Initial Movement systems added. TODO: Add jumping, walljumping,
    wallrunning, general game mechanics.

---
+++ HyperRunProject/Assets/Scripts/CCMovementScript.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class CCMovementScript : MonoBehaviour
+{
+
+   
+    public Transform CameraTransform;
+    // public float speed = 2.0f;
+    public float topSpeed = 10f;
+    public float speed = 1.0f;
+    public float jumpSpeed = 1.0f;
+    public float gravity = 20.0f;
+    float drag = 50.0f;
+    float diagonalDrag = 75.0f;
+
+    private bool isMoving = false;
+    private Vector3 moveDirection = Vector3.zero;
+    private Vector3 velocity = Vector3.zero;
+    private Rigidbody myrigidbody;
+    private CharacterController controller;
+
+    // Update is called once per frame
+    void Start()
+    {
+        myrigidbody = this.GetComponent<Rigidbody>();
+        myrigidbody.freezeRotation = true;
+        controller = this.GetComponent<CharacterController>();
+   
+    }
+  
+    bool IsGrounded()
+    {
+        if(Physics.Raycast(transform.position, -transform.up, 2f))
+        {
+            Debug.Log("hitting");
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void Update()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal")*  speed;
+        float verticalInput = Input.GetAxisRaw("Vertical")* speed;
+
+        
+        Vector3 targetDirection = (horizontalInput * Camera.main.transform.right) + (verticalInput * Camera.main.transform.forward);
+        moveDirection = Vector3.RotateTowards(moveDirection, targetDirection, 200 * Mathf.Deg2Rad * Time.deltaTime, 1000);
+
+        if (horizontalInput > 0 && verticalInput > 0)
+        {
+            velocity = Vector3.MoveTowards(velocity, Vector3.zero, diagonalDrag * Time.deltaTime);
+        }
+        else if (horizontalInput > 0 && verticalInput < 0)
+        {
+            velocity = Vector3.MoveTowards(velocity, Vector3.zero, diagonalDrag * Time.deltaTime);
+        }
+        else if (horizontalInput < 0 && verticalInput > 0)
+        {
+            velocity = Vector3.MoveTowards(velocity, Vector3.zero, diagonalDrag * Time.deltaTime);
+        }
+       else  if (horizontalInput < 0 && verticalInput < 0)
+        {
+            velocity = Vector3.MoveTowards(velocity, Vector3.zero, diagonalDrag * Time.deltaTime);
+        }
+        else
+        {
+            velocity = Vector3.MoveTowards(velocity, Vector3.zero, drag * Time.deltaTime);
+        }
+        velocity += moveDirection;
+        if(velocity.magnitude > topSpeed)
+        {
+            velocity = velocity.normalized * topSpeed;
+        }
+      
+        controller.Move((velocity + new Vector3 (0.0f, -9.8f, 0.0f))* Time.deltaTime);
+        Debug.Log(velocity);
+
+
+        if (targetDirection != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(moveDirection);
+        }
+
+    }
+}
+
+
---
+++ HyperRunProject/Assets/Scripts/MovementScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementScript : MonoBehaviour {
+
+    float movementSpeed = 1.0f;
+    float rotationSpeed = 100.0f;
+    float translation;
+    float maxSpeed = 7.0f;
+    Vector3 velocity = Vector3.zero;
+
+      // Use this for initialization
+      void Start () {
+
+      }
+
+      // Update is called once per frame
+      void Update () {
+        translation = this.transform.forward.z * movementSpeed;
+       if(Input.GetButton("Vertical") && Input.GetAxisRaw("Vertical") > 0)
+        {
+            if (movementSpeed < 20.0f)
+            {
+                movementSpeed += 0.1f;
+            }
+            translation = Input.GetAxis("Vertical") * movementSpeed;
+        }
+       
+
+        if (Input.GetAxisRaw("Vertical") == 0)
+        {
+            if (movementSpeed > 0)
+            {
+                movementSpeed -= 0.5f;
+            }
+        }
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        this.transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+    }
+
+}
+  
+
+
